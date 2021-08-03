79e46bb07c23dbf5739416815746ede9326dcb70
GeekMasher <geekmasher@gmail.com>
2021-08-03 22:10:13 +0100

    Fix build script issues

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,15 +5,15 @@ PUBLIC="./public"
 mkdir -p "$PUBLIC/presentations"
 
 for f in ./presentations/*/slides.md; do
-    echo "[+] Processing: $f"
+    if [ -f "$f" ]; then
+        echo "[+] Processing: $f"
 
-    FOLDER=$( dirname "${f}" )
-    OUTPUT="$PUBLIC/presentations/$(basename $FOLDER)"
+        FOLDER=$( dirname "${f}" )
+        OUTPUT="$PUBLIC/presentations/$(basename $FOLDER)"
 
-    if [[ -d $OUTPUT ]]; then
-        echo "[!] Directories not supported: $OUTPUT"
-    else
         echo "[+] Output :: $OUTPUT"
         marp --engine ./src/engine.js --output "$OUTPUT.html" $f
+    else
+        echo "[!] Unable to process file"
     fi
 done
