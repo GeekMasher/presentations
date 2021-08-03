9eb697b2f56bf117c239d091e622f8dd5771d7b0
Mathew Payne <geekmasher@gmail.com>
2021-08-03 22:46:50 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,26 @@
 
 PUBLIC="./public"
 
-mkdir -p "$PUBLIC/presentations"
+mkdir -p "$PUBLIC"
 
 for f in ./presentations/*/slides.md; do
     if [ -f "$f" ]; then
-        echo "[+] Processing: $f"
+        echo "[+] Processing :: $f"
 
         FOLDER=$( dirname "${f}" )
+        ASSETS="$FOLDER/assets"
         OUTPUT="$PUBLIC/$(basename $FOLDER)"
 
         echo "[+] Output :: $OUTPUT"
-        node node_modules/@marp-team/marp-cli/marp-cli.js --engine ./src/engine.js --output "$OUTPUT.html" $f
+        echo "[+] Assets :: $ASSETS"
+
+        mkdir -p "$OUTPUT/assets"
+        cp $ASSETS/*.{png,jpg,jpeg} $OUTPUT/assets
+
+        node node_modules/@marp-team/marp-cli/marp-cli.js \
+            --engine ./src/engine.js \
+            --output "$OUTPUT/index.html" \
+            $f
     else
         echo "[!] Unable to process file"
     fi
