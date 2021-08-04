b42d677dfe453386a834c7cb107f2b9298c39163
GeekMasher <geekmasher@gmail.com>
2021-08-04 21:42:44 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 PUBLIC="./public"
+VERCEL="./.vercel"
 
-mkdir -p "$PUBLIC"
+if [[ -d $PUBLIC ]]; then
+    rm -r $PUBLIC
+fi
+
+mkdir -p "$PUBLIC" "./public/common"
+
+if [[ -d $VERCEL ]]; then
+    cp -r $VERCEL "$PUBLIC/.vercel"
+fi
+
+
+ASSETS="./public/assets"
+cp ./presentations/common/*.{png,jpg,jpeg,svg} ./public/common
 
 echo "[+] Public :: $PUBLIC"
 
@@ -11,19 +24,16 @@ for f in ./presentations/*/slides.md; do
         echo "[+] Processing :: $f"
 
         FOLDER=$( dirname "${f}" )
-        ASSETS="$FOLDER/assets"
+        
         OUTPUT="$PUBLIC/$(basename $FOLDER)"
 
         echo "[+] Output :: $OUTPUT"
         echo "[+] Assets :: $ASSETS"
 
         mkdir -p "$OUTPUT/assets"
-        cp $ASSETS/*.{png,jpg,jpeg} $OUTPUT/assets
+        cp $ASSETS/*.{png,jpg,jpeg,svg} $OUTPUT/assets
 
-        node node_modules/@marp-team/marp-cli/marp-cli.js \
-            --engine ./src/engine.js \
-            --output "$OUTPUT/index.html" \
-            $f
+        marp --engine ./src/engine.js --output "$OUTPUT/index.html" $f
     else
         echo "[!] Unable to process file"
     fi
