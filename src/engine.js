184a28c4261da37d031ede59086b3f432ec8eb28
GeekMasher <geekmasher@gmail.com>
2024-08-05 12:43:15 +0100

    feat(engine): Update engine and theme

--- src/engine.js
+++ src/engine.js
@@ -10,92 +10,92 @@ const optionsInclude = {
 };
 
 var codeql = function(hljs) {
-  const regex = hljs.regex;
-  const RESERVED_WORDS = [
-    'and',
-    'any',
-    'or',
-    'none',
-    'import',
-    'in',
-    'class',
-    'extends',
-    'instanceof',
-    'exists',
-    'predicate',
-    'from',
-    'where',
-    'select',
-  ];
- 
-  const BUILT_INS = [
-    'this',
-  ];
-  const LITERALS = [
-    'true', 'false'
-  ];
-  const TYPES = [
-    'boolean',
-    'int'
-  ]
+    const regex = hljs.regex;
+    const RESERVED_WORDS = [
+        'and',
+        'any',
+        'or',
+        'none',
+        'import',
+        'in',
+        'class',
+        'extends',
+        'instanceof',
+        'exists',
+        'predicate',
+        'from',
+        'where',
+        'select',
+    ];
 
-  const KEYWORDS = {
-    $pattern: /[A-Za-z]\w+|__\w+__/,
-    keyword: RESERVED_WORDS,
-    built_in: BUILT_INS,
-    literal: LITERALS,
-    type: TYPES
-  };
-
-  
-  return {
-    name: 'CodeQL',
-    aliases: [
-      'ql',
-      'qll'
-    ],
-    unicodeRegex: true,
-    keywords: KEYWORDS,
-    illegal: /(<\/|\?)|=>/,
-    contains: [
-      // Comments
-      hljs.COMMENT(
-        '/\\*\\*',
-        '\\*/',
-        {
-          relevance: 0,
-          contains: [
-            {
-              // eat up @'s in emails to prevent them to be recognized as doctags
-              begin: /\w+@/,
-              relevance: 0
-            },
-            {
-              className: 'doctag',
-              begin: '@[A-Za-z]+'
-            }
-          ]
-        }
-      ),
-      hljs.C_LINE_COMMENT_MODE,
-      hljs.C_BLOCK_COMMENT_MODE,
-      // Imports
-      {
-        begin: /import semmle\.[a-z]+\./,
-        keywords: "import",
-        relevance: 2
-      },
-      // Strings
-      {
-        begin: /"""/,
-        end: /"""/,
-        className: "string",
-        contains: [ hljs.BACKSLASH_ESCAPE ]
-      },
-      hljs.APOS_STRING_MODE,
-      hljs.QUOTE_STRING_MODE,
+    const BUILT_INS = [
+        'this',
+    ];
+    const LITERALS = [
+        'true', 'false'
+    ];
+    const TYPES = [
+        'boolean',
+        'int'
     ]
-  };
+
+    const KEYWORDS = {
+        $pattern: /[A-Za-z]\w+|__\w+__/,
+        keyword: RESERVED_WORDS,
+        built_in: BUILT_INS,
+        literal: LITERALS,
+        type: TYPES
+    };
+
+
+    return {
+        name: 'CodeQL',
+        aliases: [
+            'ql',
+            'qll'
+        ],
+        unicodeRegex: true,
+        keywords: KEYWORDS,
+        illegal: /(<\/|\?)|=>/,
+        contains: [
+            // Comments
+            hljs.COMMENT(
+                '/\\*\\*',
+                '\\*/',
+                {
+                    relevance: 0,
+                    contains: [
+                        {
+                            // eat up @'s in emails to prevent them to be recognized as doctags
+                            begin: /\w+@/,
+                            relevance: 0
+                        },
+                        {
+                            className: 'doctag',
+                            begin: '@[A-Za-z]+'
+                        }
+                    ]
+                }
+            ),
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.C_BLOCK_COMMENT_MODE,
+            // Imports
+            {
+                begin: /import semmle\.[a-z]+\./,
+                keywords: "import",
+                relevance: 2
+            },
+            // Strings
+            {
+                begin: /"""/,
+                end: /"""/,
+                className: "string",
+                contains: [hljs.BACKSLASH_ESCAPE]
+            },
+            hljs.APOS_STRING_MODE,
+            hljs.QUOTE_STRING_MODE,
+        ]
+    };
 }
 
 // https://www.npmjs.com/package/markdown-it-highlightjs
